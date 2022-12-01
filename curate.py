cab08e65f88745c186d4fcf9a4e0d7945425b849
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2022-12-01 08:03:24 -0300

    Add ifmain

--- curate.py
+++ curate.py
@@ -1,24 +1,30 @@
+from black import main
 import pandas as pd
 import json
 from wdcuration.wdcuration import check_and_save_dict
 from pathlib import Path
 
-HERE = Path(__file__).parent.resolve()
 
-df = pd.read_csv("cl_clean.csv")
-previous_cl_matches = json.loads(HERE.joinpath("cl_on_wikidata.json").read_text())
+def main():
+    HERE = Path(__file__).parent.resolve()
+    df = pd.read_csv("cl_clean.csv")
+    previous_cl_matches = json.loads(HERE.joinpath("cl_on_wikidata.json").read_text())
 
-for i, row in df.iterrows():
-    if row["id"] in previous_cl_matches:
-        continue
-    else:
-        master_dict = {"cl_on_wikidata": previous_cl_matches}
+    for i, row in df.iterrows():
+        if row["id"] in previous_cl_matches:
+            continue
+        else:
+            master_dict = {"cl_on_wikidata": previous_cl_matches}
 
-        previous_cl_matches = check_and_save_dict(
-            master_dict,
-            "cl_on_wikidata",
-            path=HERE,
-            string=row["name"],
-            dict_key=row["id"],
-            excluded_types=["Q13442814", "Q2996394"],
-        )
+            previous_cl_matches = check_and_save_dict(
+                master_dict,
+                "cl_on_wikidata",
+                path=HERE,
+                string=row["name"],
+                dict_key=row["id"],
+                excluded_types=["Q13442814", "Q2996394"],
+            )
+
+
+if __name__ == "__main__":
+    main()
--- reconcile.py
+++ reconcile.py
@@ -6,21 +6,27 @@ import json
 from wdcuration.wdcuration import check_and_save_dict
 from pathlib import Path
 
-HERE = Path(__file__).parent.resolve()
 
-all_cl_matches = json.loads(HERE.joinpath("cl_on_wikidata.json").read_text())
+def main():
+    HERE = Path(__file__).parent.resolve()
 
-existing_cl_matches = query_wikidata(
-    '  SELECT DISTINCT ?id   (REPLACE(STR(?item), ".*Q", "Q") AS ?qid)  WHERE { ?item wdt:P7963 ?id . } '
-)
+    all_cl_matches = json.loads(HERE.joinpath("cl_on_wikidata.json").read_text())
 
-existing_cl_matches_dict = {}
-for a in existing_cl_matches:
-    existing_cl_matches_dict[a["id"]] = a["qid"]
+    existing_cl_matches = query_wikidata(
+        '  SELECT DISTINCT ?id   (REPLACE(STR(?item), ".*Q", "Q") AS ?qid)  WHERE { ?item wdt:P7963 ?id . } '
+    )
 
-new_keys = all_cl_matches.keys() - existing_cl_matches_dict.keys()
-qs = ""
-for key in new_keys:
-    qs += f'{all_cl_matches[key]}|P7963|"{key}"' + "\n"
+    existing_cl_matches_dict = {}
+    for a in existing_cl_matches:
+        existing_cl_matches_dict[a["id"]] = a["qid"]
 
-print(qs)
+    new_keys = all_cl_matches.keys() - existing_cl_matches_dict.keys()
+    qs = ""
+    for key in new_keys:
+        qs += f'{all_cl_matches[key]}|P7963|"{key}"' + "\n"
+
+    print(qs)
+
+
+if __name__ == "__main__":
+    main()
