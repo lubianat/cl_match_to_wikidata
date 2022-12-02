221e806984f1aafe77079b887ca2b99fe0bcba81
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2022-12-02 14:57:07 -0300

    Start implementing xref support

--- curate.py
+++ curate.py
@@ -6,6 +6,19 @@ from pathlib import Path
 import random
 
 
+
+
+
+
+
+
+def convert_xref_to_wikidata(xref)
+    "Converts an xref to a property value pair for Wikidata"
+
+    if 
+
+
+
 def main():
     HERE = Path(__file__).parent.resolve()
     df = pd.read_csv("cl_clean.csv")
@@ -15,13 +28,23 @@ def main():
 
     ids_to_add = sorted(ids_to_add)
 
-    random.shuffle(ids_to_add)
+    #random.shuffle(ids_to_add)
 
     try:
         for id_to_add in ids_to_add:
 
             row = df[df["id"] == id_to_add]
             master_dict = {"cl_on_wikidata": previous_cl_matches}
+
+            id_property_value_pairs = {"P7963": [row["id"].item()]}
+
+            for xref in row["xref"].split("|"):
+
+              key, value = convert_xref_to_wikidata(xref)
+              if key in id_property_value_pairs:
+                id_property_value_pairs[key].append(value)
+              else: 
+                id_property_value_pairs[key] = value
 
             dict_and_key = WikidataDictAndKey(
                 master_dict=master_dict,
@@ -32,9 +55,8 @@ def main():
                 new_item_config=NewItemConfig(
                     labels={"en": row["name"].item()},
                     descriptions={"en": "cell type"},
-                    id_property="P7963",
-                    id_value=row["id"].item(),
-                    item_property_value_pairs={"P31": "Q189118"},
+                    id_property_value_pairs=id_property_value_pairs,
+                    item_property_value_pairs={"P31": ["Q189118"]},
                 ),
                 excluded_types=["Q13442814", "Q2996394", "Q112193867", "Q187685"],
             )
